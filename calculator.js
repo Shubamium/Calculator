bd603ad976b37f902aead04c237a4e7527cce389
Vincent Liuss <liusvinv@gmail.com>
2022-12-11 03:41:50 +0700

    Add another branch for the react version

---
+++ calculator.js
@@ -0,0 +1,32 @@
+// Globals
+const elDisplay = document.getElementById('display');
+let elInputs = document.getElementsByClassName('handleInput');
+
+function handleInput(val){
+    if(typeof val !== "string") return;
+    elDisplay.value = elDisplay.value + val;
+}
+
+function clearInput(){
+    elDisplay.value = "";
+}
+
+function backspaceInput(){
+    if(elInputs.value === "")return;
+    let newInput = elDisplay.value.split('');
+    newInput.pop();
+    elDisplay.value = newInput.join('');
+}
+
+function calculate(){
+    if(elDisplay.value === "") return;
+    let convertedVal = elDisplay.value.replace(/x/gi,"*").replace(/÷/,"/");
+    elDisplay.value = eval(convertedVal);
+}
+
+for(input of elInputs){
+    let newVal = input.dataset.calcVal;
+    input.addEventListener('click', ()=>{
+        handleInput(newVal);
+    });
+}
