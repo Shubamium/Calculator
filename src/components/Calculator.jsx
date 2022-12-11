bb74a9197c9f52be02a59a87a5a4d6b5ef26f9fc
Vincent Liuss <liusvinv@gmail.com>
2022-12-11 21:47:05 +0700

    Add the structures for the calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -7,7 +7,28 @@ const Calculator = () => {
             <div className="container">
                  <Display />
                  <div className="input">
-                    <Button />
+                    <div className="numbers">
+                        <Button text="1"/>
+                        <Button text="2"/>
+                        <Button text="3"/>
+                        <Button text="4"/>
+                        <Button text="5"/>
+                        <Button text="6"/>
+                        <Button text="7"/>
+                        <Button text="8"/>
+                        <Button text="9"/>
+                        <Button text="."/>
+                        <Button text="0"/>
+                        <Button text="del"/>
+                    </div>
+                    <div className="symbols">
+                        <Button text="+"/>
+                        <Button text="-"/>
+                        <Button text="*"/>
+                        <Button text="/"/>
+                    </div>
+                    <Button text="="/>
+                    <Button text="Clear"/>
                  </div>
             </div>
         </div> 
